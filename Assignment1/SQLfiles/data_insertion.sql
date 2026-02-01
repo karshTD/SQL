83c2a4a58528a5b2a49e67993440cb7e484ab103
Utkarsh <132635254+karshTD@users.noreply.github.com>
2026-02-01 17:30:51 +0530

    Create data_insertion.sql

---
+++ Assignment1/SQLfiles/data_insertion.sql
@@ -0,0 +1,9 @@
+-- Sample INSERT statements for SIULIBRARY
+INSERT INTO SIULIBRARY (Slid, lname, location, noofbranches) VALUES
+(1, 'Central Library', 'Main Campus', 5),
+(2, 'East Wing Library', 'East Campus', 3),
+(3, 'West Wing Library', 'West Campus', 2),
+(4, 'North Library', 'North Campus', 4),
+(5, 'South Library', 'South Campus', 3);
+
+-- Continue for all other tables...
