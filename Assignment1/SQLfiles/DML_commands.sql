285b0500acc37a6a980ba352a5681730beba1fa5
Utkarsh <132635254+karshTD@users.noreply.github.com>
2026-02-01 17:30:15 +0530

    Add DML commands syntax to DML_commands.sql

---
+++ Assignment1/SQLfiles/DML_commands.sql
@@ -0,0 +1,19 @@
+-- DML Commands Syntax:
+
+-- 1. INSERT - Add new records
+INSERT INTO table_name (column1, column2) 
+VALUES (value1, value2);
+
+-- 2. UPDATE - Modify existing records
+UPDATE table_name 
+SET column1 = value1 
+WHERE condition;
+
+-- 3. DELETE - Remove records
+DELETE FROM table_name 
+WHERE condition;
+
+-- 4. SELECT - Retrieve data
+SELECT column1, column2 
+FROM table_name 
+WHERE condition;
