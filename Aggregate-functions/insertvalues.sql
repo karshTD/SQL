bad0d6b31b927bee6dc69e4fbb3a6f8b79b5d39d
Utkarsh <132635254+karshTD@users.noreply.github.com>
2026-02-12 07:12:01 +0530

    Create insertvalues.sql

---
+++ Aggregate-functions/insertvalues.sql
@@ -0,0 +1,12 @@
+INSERT INTO StudentScores (student_id, subject, marks) VALUES
+(1, 'Math', 78),
+(1, 'DSA', 85),
+(1, 'DBMS', 72),
+
+(2, 'Math', 64),
+(2, 'DSA', 91),
+(2, 'DBMS', 88),
+
+(3, 'Math', 55),
+(3, 'DSA', 60),
+(3, 'DBMS', 58);
